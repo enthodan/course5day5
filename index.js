4613ee5c2743b3ad6c4e8dbff82713a8d5ea6298
enthodan <enthodan@gmail.com>
2018-11-13 15:58:48 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,99 +1,214 @@
-/*
-  Review on Arrow functions and Map
+let main = document.querySelector('#main');
 
-  Arrow functions:
-    A way to more cleanly write anonymous functions within our code
-    Pretty much the only difference is how 'this' gets bound within an arrow function
+// Find unsorted pairs in an array
+function promiseIsSorted(arr) {
+  let promise = new Promise((resolve, reject) => {
+    let unsortedPairs = [];
 
-  Format:
-    Does not require a function keyword, or a function name. Just the parameter list and the execution(with an arrow '=>' placed between).
-  
-  // ES5 function
-  function isOdd(number){
-    if(number % 1 === 0){
-      return true;
-    }else{
-      return false;
-    }
-  }
-  // function expression
-  const isOdd = function(number){
-    . . .
-  }
-  // ES6 arrow function
-  const isOdd = (number) => {
-    . . .
-  }
-
-
-  ES6 Array method structure:
-    Functions:
-      map - Returns an array of the same length as the original
-      filter - Returns an array of the same or less length as the original
-      reduce - Returns 1 and only 1 item
-      forEach*
-      some*
-      find*
-
-    Each function takes a callback. The number of parameters for this callback are 1-3
-      1: Current item you're at in the array
-      2: Current index you're at in the array
-      3: Entire array(THE ORIGINAL array, not a copy)
-    
-    For map, the return statement in the callback will be what is placed into the new array at that index.
-    If you leave off a return statement for any item, it will be 'undefined' in the resulting array.
-    If you modify the original array to remove items after you start the map, you will still get the number of items back that you started with. Some may be empty or undefined however.
-
-    let nums = [1,2,3,4,5]
-    nums.map((item, index, array) => {
-      if(index !== 3){
-        return item;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        unsortedPairs.push([arr[i], arr[i + 1]]);
       }
+    }
+    if (unsortedPairs.length === 0) {
+      // Its sorted
+      resolve(arr);
+    } else {
+      // Not sorted
+      reject(unsortedPairs);
+    }
+  });
+
+  promise
+    .then(arr => {
+      // Print the entire array, 5 items per line
+      main.innerHTML = 'Array is sorted!\n';
+      let paragraph = document.createElement('p');
+      paragraph.innerHTML = arr[0];
+      for (let i = 1; i < arr.length; i++) {
+        if (i % 5 === 0) {
+          main.appendChild(paragraph);
+          paragraph = document.createElement('p');
+          paragraph.innerHTML = arr[i];
+          i++;
+        }
+        paragraph.innerHTML += `, ${arr[i]}`;
+      }
+
+      main.appendChild(paragraph);
     })
+    .catch(unsortedPairs => {
+      // Print the array, 3 PAIRS per line
+      main.innerHTML = 'Array is not sorted, here are the unsorted pairs!\n';
+      let paragraph = document.createElement('p');
+      paragraph.innerHTML = `[${unsortedPairs[0][0]}, ${unsortedPairs[0][1]}]`;
+      for (let i = 1; i < unsortedPairs.length; i++) {
+        if (i % 3 === 0) {
+          main.appendChild(paragraph);
+          paragraph = document.createElement('p');
+          paragraph.innerHTML = `[${unsortedPairs[i][0]}, ${
+            unsortedPairs[i][1]
+          }]`;
+          i++;
+        }
+        paragraph.innerHTML += `, [${unsortedPairs[i][0]}, ${
+          unsortedPairs[i][1]
+        }]`;
+      }
 
-*/
-
-// Recreating the map function - Callbacks and all
-
-// Old way:
-let newArr = [];
-for (let i = 0; i < oldArr.length; i++) {
-  // perform operation to add to new array
+      main.appendChild(paragraph);
+    });
 }
-return newArr;
 
-// Rebuilding the method that map uses:
-// Our version will require two parameters instead of one, because we have to feed in the array ourselves.
-function ourMap(arr, callback) {
-  // Within our map we will need the new array, a loop for the old array, and to return the new array
-  let newArr = [];
+// promiseIsSorted([2, 1, 3, 7, 6, 4, 3, 2, 1]);
+// promiseIsSorted([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
 
-  for (let i = 0; i < arr.length; i++) {
-    // This is where the logic for the new array normally goes.
-    // HOWEVER we have a callback that will handle that for us.
+function processOne() {
+  let input = JSON.parse(
+    `[${document.querySelector('#exerciseOneInput').value}]`
+  );
 
-    // All that we're responsible for is adding items to the new array
-    newArr.push(callback(arr[i], i, arr));
+  promiseIsSorted(input);
+}
+
+function containsNumber(string, callback1, callback2) {
+  let containsNum = false;
+  let firstIndex = 0;
+
+  let num = string.match(/\d+/g);
+
+  if (num) {
+    containsNum = true;
+    firstIndex = string.indexOf(num);
   }
 
-  return newArr;
-}
-let nums = [1, 2, 3, 4, 5];
-ourMap(nums, item => {
-  return item * 2;
-});
+  // Check for a number within ‘string’
+  // for (let i = 0; i < string.length; i++) {
+  //   // if (Number(string[i]) !== NaN) {
+  //   if (!isNaN(string[i])) {
+  //     // Not NOT a number
+  //     containsNum = true;
+  //     firstIndex = i;
+  //     break;
+  //   }
+  // }
 
-let newNums = [];
-for (let i = 0; i < 1000000; i++) {
-  newNums.push(i);
+  if (containsNum) {
+    callback1(string, firstIndex);
+  } else {
+    callback2(string);
+  }
+}
+function containedNumber(str, index) {
+  console.log(`${str} contained a number at index ${index}!`);
+}
+function noNumber(str) {
+  console.log(`${str} does not contain a number.`);
 }
 
-function testInputs(arr) {
-  let start = performance.now();
-  ourMap(arr, item => item * 5);
-  let mid = performance.now();
-  console.log(mid - start); // How long our version took
-  arr.map(item => item * 5);
-  let last = performance.now() - mid;
-  console.log(last); //
+function containsNumberPromise(string) {
+  let promise = new Promise((resolve, reject) => {
+    let containsNum = false;
+    let firstIndex = 0;
+
+    let num = string.match(/\d+/g);
+
+    if (num) {
+      containsNum = true;
+      firstIndex = string.indexOf(num);
+    }
+    // Built an answer/response object to pass more than one piece of information as a single parameter
+    let answer = {
+      str: string,
+      index: firstIndex
+    };
+
+    if (containsNum) {
+      resolve(answer);
+    } else {
+      reject(string);
+    }
+  });
+
+  promise
+    .then(result => {
+      console.log(`${result.str} contained a number at index ${result.index}!`);
+    })
+    .catch(str => {
+      console.log(`${str} does not contain a number.`);
+    });
 }
+
+function processTwoCallback() {
+  let input = document.querySelector('#exerciseTwoInput').value;
+  containsNumber(input, containedNumber, noNumber);
+}
+function processTwoPromise() {
+  let input = document.querySelector('#exerciseTwoInput').value;
+  containsNumberPromise(input);
+}
+
+//
+//
+//
+//
+//function usersLongestPost(id) {
+//    let userid = id;
+//    let postPromise = new Promise((resolve, reject) => {
+//        const xhr = new XMLHttpRequest();
+//        xhr.onload = () => {
+//            if (xhr.status == 200) {
+//                resolve(xhr.responseText);
+//            } else {
+//                reject(xhr.status);
+//            }
+//        }
+//        console.log('after xhr onload')
+//        xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts', true);
+//        xhr.send();
+//    })
+//    
+//    postPromise.then((response) => {
+//        let obj = JSON.parse(response);
+//        let posts = [];
+//        let biggest = 0;
+//        for (let i = 0; i < obj.length; i++) {
+//            if (obj[i].userId == userid) {
+//                posts.push(obj[i].body);
+//            }   
+//        }
+//        for (let i = 0; i < posts.length; i++) {
+//            console.log(posts)
+//            console.log('post' + i + 'has a length of' + posts[i].length)
+//            if (posts[i].length > posts[biggest].length) {
+//                biggest = i;
+//            }
+//        }
+//        
+//        let id = biggest+1;
+//        
+//        console.log(`Post id ${id} is the longest post from user id ${userid}`);
+//    })
+//    .catch((status) => console.log('u suck and here is why' + status ))
+//}
+//
+//usersLongestPost(2);
+
+
+
+function getGit(userid) {
+    let url = 'https://api.github.com/users/' + userid;
+    fetch(url)
+    .then((response) => response.json())
+    .then((json) => {console.log(`created at ${json.created_at} and has ${json.followers} followers. So popular.`);
+                    return json.followers_url})
+    .then((url) => fetch(url))
+    .then((response) => response.json())
+    .then((json) => console.log(json.map(e => {
+        return e.login;
+    }
+    )))
+
+}
+
+getGit('finnickychief')
